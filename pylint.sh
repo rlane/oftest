2221409c963ace1edb570573599bdd01e2508fe2
Rich Lane <rlane@bigswitch.com>
2013-08-23 12:40:25 -0700

    add coding style checks to pylint.sh

--- pylint.sh
+++ pylint.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 cd $(dirname $(readlink -f $0))
-PYTHONPATH=src/python:tests:tests-1.3 pylint -E tests/*.py tests-1.3/*.py src/python/oftest/*.py
+PYLINT_FLAGS="--include-ids=yes "
+PYLINT_FLAGS+="--errors-only " # disable all warnings, only report errors
+PYLINT_FLAGS+="--enable=format " # enable formatting style checks
+PYLINT_FLAGS+="--disable=C0301 " # disable line too long warnings
+PYLINT_FLAGS+="--disable=C0302 " # disable module too long warnings
+PYTHONPATH=src/python:tests:tests-1.3 pylint $PYLINT_FLAGS tests/*.py tests-1.3/*.py src/python/oftest/*.py
